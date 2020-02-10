7088873faa60059096646d09185e628a1d103849
alu0101021768 <alu0101021768@ull.edu.es>
2020-02-10 22:03:19 +0000

    Retocada

--- P1/Vernam.cpp
+++ P1/Vernam.cpp
@@ -4,66 +4,77 @@
 #include <sstream>
 using namespace std;
 
+string to_bin(string mensaje) {
+    string aux;
+      for (int i = 0; i < mensaje.size(); ++i)
+    {
+        aux += bitset<8>(mensaje[i]).to_string();
+    }
+    return aux;
+}
+
+string xor_add(string mensaje_bin , string clave) {
+    string result;
+    for (int i = 0; i < mensaje_bin.size(); i++)
+    {
+        if (mensaje_bin[i] == clave[i])
+            result += '0';
+        else
+            result += '1';
+    }
+    return result;
+}
+
+string bin_to_alpha(string mensaje_cifrado) {
+    string result;
+    istringstream palabra(mensaje_cifrado);
+    bitset<8> letra;
+    while (palabra >> letra)
+    {
+        result += char(letra.to_ulong());   // Pasamos cada letra en binario a ascii
+    }
+    return result;
+}
+
 int main()
 {
 
     string mensaje, clave, result, mensaje_bin = "";
-
+    
+    // Leemos por teclado el mensaje
     cout << "Introduzca el mensaje deseado : ";
     cin >> mensaje;
-
-    for (int i = 0; i < mensaje.size(); ++i)
-    {
-        mensaje_bin += bitset<8>(mensaje[i]).to_string();
-    }
-
+    
+    // Pasamos el mensaje a binario
+    mensaje_bin = to_bin(mensaje);
     cout << "Mensaje original en binario   : " << mensaje_bin << endl;
+
+    // Leemos por teclado la clave
     cout << "Introduzca la clave aleatoria : ";
     cin >> clave;
 
-    for (int i = 0; i < mensaje_bin.size(); i++)
-    {
-        if (mensaje_bin[i] == clave[i])
-            result += '0';
-        else
-            result += '1';
-    }
+    // Se realiza una XOR entre el mensaje original y la clave , cifrándolo
+    result = xor_add(mensaje_bin , clave);
     cout << "Mensaje cifrado en binario : " << result << endl;
-    istringstream in(result);
-    bitset<8> fin;
-    result.clear();
-    while (in >> fin)
-    {
-        result += char(fin.to_ulong());
-    }
-    cout << "Mensaje cifrado : " << result << endl;
-    string aux = result;
-    mensaje_bin.clear();
-    for (int i = 0; i < result.size(); ++i)
-    {
-        mensaje_bin += bitset<8>(result[i]).to_string();
-    }
 
+    string aux = result;
     result.clear();
-    for (int i = 0; i < mensaje_bin.size(); i++)
-    {
-        if (mensaje_bin[i] == clave[i])
-            result += '0';
-        else
-            result += '1';
-    }
+    // Se pasa el mensaje cifrado en binario a ascii
+    result = bin_to_alpha(aux); 
+    cout << "Mensaje cifrado : " << result << endl;
+    
+    // Se vuelve a pasar a binario el mensaje cifrado
+    string cifrado = to_bin(result);
+
+    // Se realiza una XOR entre el mensaje cifrado en binario y la clave 
+    string resultado = xor_add(cifrado , clave);
+
     cout << "-------------------------------------------------" << endl;
-    cout << "Mensaje " << aux << " + " << "clave aleatoria  : " << mensaje_bin << endl;
-    istringstream last(result);
-    bitset<8> fin2;
+    cout << "Mensaje " << result << " + " << "clave aleatoria  : " << clave << endl;
     result.clear();
-    while (last >> fin2)
-    {
-        result += char(fin2.to_ulong());
-    }
+    
+    // Se pasa el resultado de la XOR a ascii , descifrándolo
+    result = bin_to_alpha(resultado);  
     cout << "Mensaje final descifrado      : " << result << endl;
 }
 
-// Otra opcion
-//for (int i = 0; i < mensaje_bin.size(); i++)
-//     result[i] += (mensaje_bin[i] ^ clave[i]);
