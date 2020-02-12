df39539a9e96c4d48a860df77dd713b53c6f9761
alu0101021768 <alu0101021768@ull.edu.es>
2020-02-12 01:06:03 +0000

    No funciona el decypher , porque hay que restar al mensaje cifrado la clave , en vez de sumar el mensaje con la clave , el resto dejarlo igual. Por lo tanto , si queremos usar la opción de descifrar tiene que ser después de la de descifrar

--- Practica2/P2/main.cpp
+++ Practica2/P2/main.cpp
@@ -16,7 +16,20 @@ int find_value(char caracter) {
     return val;
 }
 
-std::string add_key(std::string mensaje , std::string clave) {
+void read_mensaje(std::string& mensaje) {
+    std::cout << "Introduzca el mensaje : ";
+    getline(std::cin, mensaje);
+
+}
+
+void read_clave(std::string& clave)
+{
+    std::cout << "Introduzca la clave : ";
+    std::cin >> clave;
+   
+}
+
+std::string add_key_cifrado(std::string mensaje , std::string clave) {
 
     std::string resultado;
     resultado.resize(mensaje.size());
@@ -37,36 +50,78 @@ std::string add_key(std::string mensaje , std::string clave) {
 
 }
 
+std::string add_key_descifrado(std::string mensaje, std::string clave)
+{
+
+    std::string resultado;
+    resultado.resize(mensaje.size());
+    int tam_msg = mensaje.size();
+    int tam_clave = clave.size();
+
+    int cont = 0;
+    while (tam_clave != tam_msg)
+    {
+        clave += clave[cont];
+        cont++, tam_clave++;
+        if (cont == tam_clave)
+            cont = 0;
+    }
+    for (int i = 0; i < tam_msg; i++)
+    {
+        resultado[i] = alfabeto[((find_value(mensaje[i]) + find_value(clave[i])) + 26) % 26];
+    }
+    return resultado;
+}
+
+void delete_spaces(std::string& mensaje) {
+    mensaje.erase(remove_if(mensaje.begin(), mensaje.end(), isspace), mensaje.end());
+    std::cout << "Mensaje sin espacios : " << mensaje << std::endl;
+}
+
+void cifrado(std::string& resultado , std::string mensaje , std::string clave) {
+    resultado = add_key_cifrado(mensaje, clave);
+    std::cout << "Mensaje cifrado : " << resultado << std::endl;
+}
+
+void descifrado(std::string& resultado , std::string mensaje , std::string clave) {
+    resultado = add_key_descifrado(mensaje, clave);
+    std::cout << "Mensaje cifrado : " << resultado << std::endl;
+}
+
 int main () {
 
-    
-    // leemos el mensaje por teclado
     std::string mensaje;
-    std::cout << "Introduzca el mensaje : ";
-    getline (std::cin , mensaje);
-
-    // Mensaje sin espacios
-    mensaje.erase(remove_if(mensaje.begin() , mensaje.end() , isspace) , mensaje.end());
-    std::cout << "Mensaje sin espacios : " << mensaje << std::endl;
-    
-
-    // Leemos la clave por teclado
     std::string clave;
-    std::cout << "Introduzca la clave : ";
-    std::cin >> clave;
+    std::string resultado;
     
+    bool salir = false;
+    int opcion;
 
-    // Sumamos el mensaje con la clave , de manera que dividimos el mensaje original en trozos 
-    // del mismo tamaño que la clave , y sumamos las posiciones 0 de cada trozo con la 0 de la clave ,
-    // luego la 1 con la 1 y asi sucesivamente
-    
-    std::string resultado = add_key(mensaje , clave);
-    //std::cout << "valor de msg : " << find_value(mensaje[1]) << std::endl;
-    //std::cout << "valor de clave : " << find_value(clave[1]) << std::endl;
-    //std::cout << "MODULO DE SUMA : " << alfabeto[26 % modulo] << std::endl;
-    
-
-    // Mensaje cifrado
-    std::cout << "Mensaje cifrado : " << resultado << std::endl;
+    std::cout << "Práctica 2 - Cifrado de Vigenere" << std::endl;
+    read_mensaje(mensaje);
+    read_clave(clave);
+    std::cout << "1.Cifrado" << std::endl;
+    std::cout << "2.Descifrado" << std::endl;
+    std::cout << "3.Salir" << std::endl;
+    std::cout << "Seleccione una opción : ";
+    std::cin >> opcion;
+    switch (opcion)
+    {
+    case 1:
+        delete_spaces(mensaje);
+        cifrado(resultado, mensaje, clave);
+        break;
+    case 2:
+        delete_spaces(mensaje);
+        descifrado(resultado, mensaje, clave);
+        break;
+    case 3:
+        salir = true;
+        break;
+    default:
+        std::cout << "Opción no válida" << std::endl;
+        break;
+    }
+    std::cout << "---------------------------------------------" << std::endl;
 }
     
