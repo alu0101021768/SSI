41e9d1c2748d4cbf79257886760c741aef42f6f4
alu0101021768 <alu0101021768@ull.edu.es>
2020-02-12 19:21:31 +0000

    Práctica finalizada

--- Practica2/P2/main.cpp
+++ Practica2/P2/main.cpp
@@ -6,9 +6,13 @@
 
 // Definimos el alfabeto sin Ñ y con W
 const std::string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// Definimos el modulo
 const int modulo = 26;
 
-int find_value(char caracter) {
+// Busca en el alfabeto el valor de la letra
+int find_value(char caracter) 
+{
     int val = alfabeto.find(caracter);
     if (val == std::string::npos) {
         std::cout << "Carácter " << caracter << " no encontrado" << std::endl;   
@@ -16,12 +20,15 @@ int find_value(char caracter) {
     return val;
 }
 
-void read_mensaje(std::string& mensaje) {
+// Lee por teclado el mensaje
+void read_mensaje(std::string& mensaje) 
+{
     std::cout << "Introduzca el mensaje : ";
     getline(std::cin, mensaje);
 
 }
 
+// Lee por teclado la clave
 void read_clave(std::string& clave)
 {
     std::cout << "Introduzca la clave : ";
@@ -29,8 +36,9 @@ void read_clave(std::string& clave)
    
 }
 
-std::string add_key_cifrado(std::string mensaje , std::string clave) {
-
+// Suma el mensaje con la clave 
+std::string add_key_cifrado(std::string mensaje , std::string clave) 
+{
     std::string resultado;
     resultado.resize(mensaje.size());
     int tam_msg = mensaje.size();
@@ -47,12 +55,11 @@ std::string add_key_cifrado(std::string mensaje , std::string clave) {
         resultado[i] = alfabeto[(find_value(mensaje[i]) + find_value(clave[i])) % 26];
     }
     return resultado;
-
 }
 
-std::string add_key_descifrado(std::string mensaje, std::string clave)
-{
-
+// Resta al mensaje la clave
+std::string subtraction_key_descifrado(std::string mensaje, std::string clave) 
+{    
     std::string resultado;
     resultado.resize(mensaje.size());
     int tam_msg = mensaje.size();
@@ -68,60 +75,50 @@ std::string add_key_descifrado(std::string mensaje, std::string clave)
     }
     for (int i = 0; i < tam_msg; i++)
     {
-        resultado[i] = alfabeto[((find_value(mensaje[i]) + find_value(clave[i])) + 26) % 26];
+        resultado[i] = alfabeto[((find_value(mensaje[i]) - find_value(clave[i])) + 26) % 26];
     }
     return resultado;
 }
 
+
+// Borra los espacios
 void delete_spaces(std::string& mensaje) {
     mensaje.erase(remove_if(mensaje.begin(), mensaje.end(), isspace), mensaje.end());
     std::cout << "Mensaje sin espacios : " << mensaje << std::endl;
 }
 
+// llama a la función add key para cifrar
 void cifrado(std::string& resultado , std::string mensaje , std::string clave) {
     resultado = add_key_cifrado(mensaje, clave);
     std::cout << "Mensaje cifrado : " << resultado << std::endl;
 }
 
+// llama a la función SUBSTRACT key para descifrar
 void descifrado(std::string& resultado , std::string mensaje , std::string clave) {
-    resultado = add_key_descifrado(mensaje, clave);
-    std::cout << "Mensaje cifrado : " << resultado << std::endl;
+    resultado = subtraction_key_descifrado(mensaje, clave);
+    std::cout << "Mensaje descifrado : " << resultado << std::endl;
 }
 
 int main () {
 
     std::string mensaje;
     std::string clave;
+    std::string mensaje_cifrado;
     std::string resultado;
-    
-    bool salir = false;
-    int opcion;
 
-    std::cout << "Práctica 2 - Cifrado de Vigenere" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "| Práctica 2 - Cifrado de Vigenere |" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+  
     read_mensaje(mensaje);
     read_clave(clave);
-    std::cout << "1.Cifrado" << std::endl;
-    std::cout << "2.Descifrado" << std::endl;
-    std::cout << "3.Salir" << std::endl;
-    std::cout << "Seleccione una opción : ";
-    std::cin >> opcion;
-    switch (opcion)
-    {
-    case 1:
-        delete_spaces(mensaje);
-        cifrado(resultado, mensaje, clave);
-        break;
-    case 2:
-        delete_spaces(mensaje);
-        descifrado(resultado, mensaje, clave);
-        break;
-    case 3:
-        salir = true;
-        break;
-    default:
-        std::cout << "Opción no válida" << std::endl;
-        break;
-    }
-    std::cout << "---------------------------------------------" << std::endl;
+
+    delete_spaces(mensaje);
+
+    cifrado(mensaje_cifrado, mensaje, clave);
+
+    descifrado(resultado, mensaje_cifrado, clave);
+
+    std::cout << "-----------------------------------" << std::endl;
 }
     
---
+++ Practica2/README.md
@@ -0,0 +1,3 @@
+# Vigenere Cypher
+
+### - Vigenere Cipher is kind of polyalphabetic substitution method. It is used for encryption of alphabetic text.For encryption and decryption Vigenere Cipher Table is used in which alphabets from A to Z are written in 26 rows.
